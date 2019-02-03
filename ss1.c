a930041a9ae3214fbe73798a5c85a6e4c8e94af4
sarathkumarselvam <47300048+sarathkumarselvam@users.noreply.github.com>
2019-02-04 00:10:46 +0530

    Create ss1.c

---
+++ ss1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("enter the value:\n");
+    scanf("%d",&n);
+    if(n%2==0)
+        printf("the value is even:");
+        else
+        printf("the value is odd:");
+    return 0;
+}
