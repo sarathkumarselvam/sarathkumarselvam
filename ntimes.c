0addc4074a1d62d8493ad44d168c85961a03a67a
sarathkumarselvam <47300048+sarathkumarselvam@users.noreply.github.com>
2019-02-05 00:12:05 +0530

    Create ntimes.c

---
+++ ntimes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,n;
+    printf("enter the value:\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("sarath sp\n");
+    }
+    return 0;
+}
